2f1d2e572437e4360fa4754bfd01939b0bfa1b2f
classbinu <classbinu@gmail.com>
2023-11-20 01:52:27 +0000

    동적 페이지 html 폼 구현

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -102,10 +102,7 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
   char buf[MAXLINE], body[MAXBUF];
 
   sprintf(body, "<html><title>Tiny Error</title>");
-  sprintf(body, "%s<body bgcolor="
-                "ffffff"
-                ">\r\n",
-          body);
+  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
   sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
   sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
   sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
